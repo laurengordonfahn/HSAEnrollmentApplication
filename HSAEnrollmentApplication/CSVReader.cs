5c831effb9fe4c03859f78ede805784f8edce297
Lauren Gordon-Fahn <lgordon-fahn@MKE-LGFahn.attlocal.net>
2020-07-14 20:53:28 -0500

    building out the csv reading logic

--- HSAEnrollmentApplication/CSVReader.cs
+++ HSAEnrollmentApplication/CSVReader.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using FluentValidation;
+using FluentValidation.Results;
+
 namespace HSAEnrollmentApplication
 {
     public class CSVReader
     {
+        string CSVPath;
+        public DateTime TimeStamp = DateTime.UtcNow;
+
         public CSVReader()
         {
+
+            try
+            {
+                string[] rows = System.IO.File.ReadAllLines(CSVPath);
+            
+                foreach (string row in rows)
+                {
+                    string [] fields = row.Split(",");
+                    EnrollmentDataModel enrollment = new EnrollmentDataModel().CreateInstanceFromList(fields);
+
+                    //validate
+                    EnrollmentDataValidator validator = new EnrollmentDataValidator();
+                    ValidationResult result = validator.Validate(enrollment);
+                    // read to memorystream
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception thrown trying to read in a csv file with " +
+                   "CSVPath [" + CSVPath + "]" +
+                   "Exception [" + e + "]" +
+                   "at [" + TimeStamp + "]");
+            }
         }
     }
 }
