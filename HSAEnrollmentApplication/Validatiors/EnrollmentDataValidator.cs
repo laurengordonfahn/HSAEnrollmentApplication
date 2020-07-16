02d4c2863ace8552ea7edce0f241aaf94cdbe6b1
Lauren Gordon-Fahn <lgordon-fahn@MKE-LGFahn.attlocal.net>
2020-07-15 21:01:59 -0500

    Console Interactive Working

---
+++ HSAEnrollmentApplication/Validatiors/EnrollmentDataValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentValidation;
+using System.Globalization;
+
+namespace HSAEnrollmentApplication
+{
+    public class EnrollmentDataValidator : AbstractValidator<EnrollmentDataModel>
+    {
+        public EnrollmentDataValidator()
+        {
+            RuleFor(dataRow => dataRow.FirstName).NotEmpty();
+            RuleFor(dataRow => dataRow.LastName).NotEmpty();
+            RuleFor(dataRow => dataRow.DOB).NotEmpty()
+                .Must(dob => IsValidDate(dob));
+            RuleFor(dataRow => dataRow.PlanType).NotEmpty()
+                .Must(planType => IsValidPlanType(planType));
+            RuleFor(dataRow => dataRow.EffectiveDate).NotEmpty()
+                .Must(effectiveDate => IsValidDate(effectiveDate));
+        }
+
+        private bool IsValidDate(string date)
+        {
+
+            DateTime dateOutput;
+            string format = "MMddyyyy";
+
+            return DateTime.TryParseExact(date, format, new CultureInfo("en-US"), DateTimeStyles.None, out dateOutput);
+   
+        }
+
+
+        private bool IsValidPlanType(HSAEnrollmentApplication.PlanType planType)
+        {
+
+           return Enum.IsDefined(typeof(PlanType), planType);
+
+        }
+    }
+}
