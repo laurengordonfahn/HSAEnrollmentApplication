5eb738a0eb4b85b05e74949d8c48ea4c65c4c485
Lauren Gordon-Fahn <lgordon-fahn@MKE-LGFahn.attlocal.net>
2020-07-17 02:17:15 -0500

    Added EnrollmentAssementDataModel

--- HSAEnrollmentApplication/Models/EnrollmentAssessmentDataModel.cs
+++ HSAEnrollmentApplication/Models/EnrollmentAssessmentDataModel.cs
@@ -1,22 +1,22 @@
 ﻿using System.Collections.Generic;
 
-namespace HSAEnrollmentApplication
+namespace HSAEnrollmentApplication.Models
 {
-    public class EnrollmentDataModel
+    public class EnrollmentAssessmentDataModel
     {
+     
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string DOB { get; set; }
-        public string PlanType { get; set; }
+        public PlanType PlanType { get; set; }
         public string EffectiveDate { get; set; }
 
-        public EnrollmentDataModel (List<string> dataRow )
+        public EnrollmentAssessmentDataModel(List<string> dataRow)
         {
-            FirstName = dataRow[0];
-            LastName = dataRow[1];
             DOB = dataRow[2];
-            PlanType = dataRow[3];
             EffectiveDate = dataRow[4];
         }
+        
     }
 }
+
